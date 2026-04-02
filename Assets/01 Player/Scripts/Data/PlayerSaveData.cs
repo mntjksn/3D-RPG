4fb64fb0e6782d08a3bb8887b5caf259544203b6
Byungchang Kim <mntjksn@naver.com>
2026-04-03 02:27:21 +0900

    획득 아이템 데이터 추가(리스트)

--- Assets/01 Player/Scripts/Data/PlayerSaveData.cs
+++ Assets/01 Player/Scripts/Data/PlayerSaveData.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 [System.Serializable]
 public class PlayerSaveData
 {
     public int level;
     public int currentExp;
     public float currentHp;
+    public int gold;
+
+    public List<InventoryItemSaveData> inventoryItems = new List<InventoryItemSaveData>();
 }
