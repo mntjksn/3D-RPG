1bc027e251dd53799588b218b5110f0decd5db80
mntjksn <mntjksn@naver.com>
2026-04-21 01:39:49 +0900

    SFX 데이터 타입

---
+++ Assets/06 UI/Scripts/Data/SFXType.cs
@@ -0,0 +1,21 @@
+public enum SfxType
+{
+    None = 0,
+
+    // Combat
+    PlayerAttack,
+    PlayerShield,
+    PlayerHit,
+
+    // System
+    BuySell,
+    Equip,
+    ItemPickup,
+    Interaction,
+
+    // Consumable
+    PotionUse,
+
+    // Movement
+    Footstep
+}
---
+++ Assets/06 UI/Scripts/Data/SfxClipData.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class SfxClipData
+{
+    public SfxType type;
+    public AudioClip clip;
+}
