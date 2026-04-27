c3022b0e746eabfe9b9d4f68cb7465763647a100
mntjksn <mntjksn@naver.com>
2026-04-27 18:35:05 +0900

    UI 패널 종류

--- Assets/06 UI/Scripts/Data/UIPanelType.cs
+++ Assets/06 UI/Scripts/Data/UIPanelType.cs
@@ -1,10 +1,12 @@
+// UI 패널 종류
 public enum UIPanelType
 {
-    None,
-    Inventory,
-    PlayerInfo,
-    Shop,
-    Quest,
-    Upgrade,
-    Setting
+    None = 0,      // 열려있는 패널 없음
+
+    Inventory,     // 인벤토리
+    PlayerInfo,    // 플레이어 정보
+    Shop,          // 상점
+    Quest,         // 퀘스트
+    Upgrade,       // 업그레이드
+    Setting        // 설정
 }
