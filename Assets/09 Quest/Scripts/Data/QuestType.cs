fa1f7cddcab89226bdff96c5fd3b8b2f5e2e93a1
mntjksn <mntjksn@naver.com>
2026-04-27 18:39:32 +0900

    퀘스트 타입

--- Assets/09 Quest/Scripts/Data/QuestType.cs
+++ Assets/09 Quest/Scripts/Data/QuestType.cs
@@ -1,9 +1,10 @@
+// 퀘스트 타입
 public enum QuestType
 {
-    KillMonster,   // 몬스터 처치
-    CollectItem,   // 아이템 획득
-    BuyItem,       // 아이템 구매
-    EquipItem,     // 아이템 착용
-    SellItem,      // 아이템 판매
-    Upgrade        // 업그레이드
+    KillMonster = 0, // 몬스터 처치
+    CollectItem,     // 아이템 획득
+    BuyItem,         // 아이템 구매
+    EquipItem,       // 아이템 착용
+    SellItem,        // 아이템 판매
+    Upgrade          // 업그레이드
 }
