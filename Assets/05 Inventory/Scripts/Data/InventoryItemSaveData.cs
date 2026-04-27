47fa2c536f28a6dfe2e9786167832d5677281ff4
mntjksn <mntjksn@naver.com>
2026-04-27 18:33:45 +0900

    인벤토리 슬롯 저장 데이터

--- Assets/05 Inventory/Scripts/Data/InventoryItemSaveData.cs
+++ Assets/05 Inventory/Scripts/Data/InventoryItemSaveData.cs
@@ -1,9 +1,10 @@
 using System;
 
+// 인벤토리 슬롯 저장 데이터
 [Serializable]
 public class InventoryItemSaveData
 {
-    public int slotIndex;
-    public string itemId;
-    public int amount;
+    public int slotIndex;   // 슬롯 위치
+    public string itemId;   // 아이템 ID
+    public int amount;      // 수량
 }
