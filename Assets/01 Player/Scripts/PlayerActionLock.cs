a5f06bf958df4b77190310e1a3259fd9a7482694
mntjksn <mntjksn@naver.com>
2026-04-27 18:29:38 +0900

    플레이어 행동 가능 여부를 중앙에서 관리 (이동/공격/방어/UI 잠금)

--- Assets/01 Player/Scripts/PlayerActionLock.cs
+++ Assets/01 Player/Scripts/PlayerActionLock.cs
@@ -1,49 +1,39 @@
 using UnityEngine;
 
+// 플레이어 행동 가능 여부를 중앙에서 관리 (이동/공격/방어/UI 잠금)
 public class PlayerActionLock : MonoBehaviour
 {
-    public bool CanMove { get; private set; } = false;
-    public bool CanAttack { get; private set; } = false;
-    public bool CanShield { get; private set; } = false;
-    public bool CanLook { get; private set; } = false;
-    public bool CanUI { get; private set; } = false;
+    public bool CanMove { get; private set; }
+    public bool CanAttack { get; private set; }
+    public bool CanShield { get; private set; }
+    public bool CanLook { get; private set; }
+    public bool CanUI { get; private set; }
 
     public bool IsAttacking { get; private set; }
     public bool IsShielding { get; private set; }
 
     private void Start()
     {
+        // 로드 완료 전까지 모든 조작 잠금
         LockRecoverControls();
     }
 
+    // 모든 조작 잠금 (로딩 중, 사망 시 등)
     public void LockRecoverControls()
     {
-        CanMove = false;
-        CanAttack = false;
-        CanLook = false;
-        CanShield = false;
-        CanUI = false;
+        CanMove = CanAttack = CanLook = CanShield = CanUI = false;
     }
 
+    // 모든 조작 해제
     public void UnlockRecoverControls()
     {
-        CanMove = true;
-        CanAttack = true;
-        CanLook = true;
-        CanShield = true;
-        CanUI = true;
+        CanMove = CanAttack = CanLook = CanShield = CanUI = true;
     }
 
-    public void SetAttack(bool value)
-    {
-        IsAttacking = value;
-    }
+    public void SetAttack(bool value) => IsAttacking = value;
+    public void SetShield(bool value) => IsShielding = value;
 
-    public void SetShield(bool value)
-    {
-        IsShielding = value;
-    }
-
+    // 사망 시 호출 - 조작 잠금 및 전투 상태 초기화
     public void OnDie()
     {
         LockRecoverControls();
@@ -51,6 +41,7 @@ public class PlayerActionLock : MonoBehaviour
         IsAttacking = false;
     }
 
+    // 부활 시 호출 - 전투 상태 초기화 후 조작 해제
     public void ResetState()
     {
         IsAttacking = false;
@@ -58,7 +49,7 @@ public class PlayerActionLock : MonoBehaviour
         UnlockRecoverControls();
     }
 
-    // DieRecover 끝날 때 Animation Event로 호출
+    // DieRecover 애니메이션 종료 시 Animation Event로 호출
     public void OnRecoverFinished()
     {
         UnlockRecoverControls();
