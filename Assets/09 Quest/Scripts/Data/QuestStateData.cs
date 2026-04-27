a6f885371c874d5783b87ad27956ef81be29b158
mntjksn <mntjksn@naver.com>
2026-04-27 18:39:26 +0900

    퀘스트 진행 상태 데이터

--- Assets/09 Quest/Scripts/Data/QuestStateData.cs
+++ Assets/09 Quest/Scripts/Data/QuestStateData.cs
@@ -1,11 +1,14 @@
 using System;
 
+// 퀘스트 진행 상태 데이터
 [Serializable]
 public class QuestStateData
 {
     public string questId;
+
     public bool isAccepted;
     public bool isCompleted;
     public bool isRewardClaimed;
+
     public int currentCount;
 }
