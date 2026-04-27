9c4726ced30bdb98771754eff3e640df8e68894f
mntjksn <mntjksn@naver.com>
2026-04-27 18:32:52 +0900

    아이템 드랍 정보

--- Assets/04 Items/Scripts/DropEntry.cs
+++ Assets/04 Items/Scripts/DropEntry.cs
@@ -1,14 +1,15 @@
 using System;
 using UnityEngine;
 
+// 아이템 드랍 정보
 [Serializable]
 public class DropEntry
 {
     public ItemData itemData;
 
     [Range(0f, 1f)]
-    public float dropChance = 1f;
+    public float dropChance = 1f;   // 드랍 확률 (0~1)
 
-    public int minAmount = 1;
-    public int maxAmount = 1;
+    public int minAmount = 1;       // 최소 수량
+    public int maxAmount = 1;       // 최대 수량
 }
