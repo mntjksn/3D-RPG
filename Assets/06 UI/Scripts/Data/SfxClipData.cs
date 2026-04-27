3f98150967da2a2f2d968c6877f415d12063feb9
mntjksn <mntjksn@naver.com>
2026-04-27 18:34:54 +0900

    SFX 타입과 오디오 클립 매핑 데이터

--- Assets/06 UI/Scripts/Data/SfxClipData.cs
+++ Assets/06 UI/Scripts/Data/SfxClipData.cs
@@ -1,9 +1,10 @@
 using System;
 using UnityEngine;
 
+// SFX 타입과 오디오 클립 매핑 데이터
 [Serializable]
 public class SfxClipData
 {
-    public SfxType type;
-    public AudioClip clip;
+    public SfxType type;     // 효과음 종류
+    public AudioClip clip;   // 오디오 클립
 }
