5fb208232b1450ef6c76b8aaa715980485bf69a4
mntjksn <mntjksn@naver.com>
2026-04-27 18:36:37 +0900

    상점에 판매되는 아이템 목록

--- Assets/07 Shop/Scripts/Data/ShopData.cs
+++ Assets/07 Shop/Scripts/Data/ShopData.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ShopData", menuName = "Game/Shop Data")]
+// 상점에 판매되는 아이템 목록
 public class ShopData : ScriptableObject
 {
-    public List<ShopItemEntry> items = new List<ShopItemEntry>();
+    public List<ShopItemEntry> items = new();
 }
 
 [Serializable]
+// 상점 아이템 정보
 public class ShopItemEntry
 {
     public ItemData itemData;
