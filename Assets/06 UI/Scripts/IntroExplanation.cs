bdd5b4e5c2e22f79fef5b78aa63e96dca684132e
mntjksn <mntjksn@naver.com>
2026-04-27 18:35:24 +0900

    인트로 설명창 열기 / 닫기 담당

--- Assets/06 UI/Scripts/IntroExplanation.cs
+++ Assets/06 UI/Scripts/IntroExplanation.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
+// 인트로 설명창 열기 / 닫기 담당
 public class IntroExplanation : MonoBehaviour
 {
-    [SerializeField] private GameObject explanationPanel; // 설명창
+    [SerializeField] private GameObject explanationPanel;
 
+    // 설명창 열기
     public void Open()
     {
-        explanationPanel.SetActive(true);
+        explanationPanel?.SetActive(true);
     }
 
+    // 설명창 닫기
     public void Close()
     {
-        explanationPanel.SetActive(false);
+        explanationPanel?.SetActive(false);
     }
 }
