ccf91b67250087272af1b13349f733d211990b78
mntjksn <mntjksn@naver.com>
2026-04-27 18:40:56 +0900

    적 스포너 상태 네트워크 동기화 담당

--- Assets/11 Photon/Scripts/EnemySpawnerNetworkSync.cs
+++ Assets/11 Photon/Scripts/EnemySpawnerNetworkSync.cs
@@ -5,12 +5,16 @@ using Photon.Realtime;
 using ExitGames.Client.Photon;
 
 [RequireComponent(typeof(EnemySpawner))]
+// 적 스포너 상태 네트워크 동기화 담당
 public class EnemySpawnerNetworkSync : MonoBehaviourPunCallbacks, IOnEventCallback
 {
+    private const byte BaseActivateEventCode = 10;
+    private const byte BaseReturnEventCode = 11;
+
     private EnemySpawner spawner;
 
-    private byte ActivateEventCode => (byte)(10 + spawner.SpawnerIndex * 2);
-    private byte ReturnEventCode => (byte)(11 + spawner.SpawnerIndex * 2);
+    private byte ActivateEventCode => (byte)(BaseActivateEventCode + spawner.SpawnerIndex * 2);
+    private byte ReturnEventCode => (byte)(BaseReturnEventCode + spawner.SpawnerIndex * 2);
 
     private void Awake()
     {
@@ -34,6 +38,7 @@ public class EnemySpawnerNetworkSync : MonoBehaviourPunCallbacks, IOnEventCallba
         StartCoroutine(WaitForRoomAndSpawn());
     }
 
+    // 방 입장 후 초기 스폰
     private IEnumerator WaitForRoomAndSpawn()
     {
         yield return new WaitUntil(() => PhotonNetwork.InRoom);
@@ -48,130 +53,129 @@ public class EnemySpawnerNetworkSync : MonoBehaviourPunCallbacks, IOnEventCallba
         yield return null;
 
         spawner.SpawnInitialEnemiesAndBroadcast();
-
-        // 처음 스폰된 적들을 참가자에게도 보내야 하니까
-        // SpawnInitialEnemies 내부에서 직접 브로드캐스트를 안 하니
-        // 현재 활성 상태를 전체에게 다시 뿌려줌
-        BroadcastCurrentStateToOthers();
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        Debug.Log($"[Spawner_{spawner.SpawnerIndex}] 새 플레이어 입장: {newPlayer.NickName}");
-
         if (!PhotonNetwork.IsMasterClient)
             return;
 
         StartCoroutine(SendCurrentStateRoutine(newPlayer));
     }
 
+    // 새 플레이어에게 현재 상태 전송
     private IEnumerator SendCurrentStateRoutine(Player newPlayer)
     {
-        Debug.Log($"[Spawner_{spawner.SpawnerIndex}] 상태 전송 시작");
         yield return new WaitForSeconds(1f);
-
         spawner.SendCurrentStateToPlayer(newPlayer);
-
-        Debug.Log($"[Spawner_{spawner.SpawnerIndex}] 상태 전송 완료");
     }
 
+    // 적 생성 브로드캐스트
     public void BroadcastActivate(int dataIndex, int poolIndex, Vector3 spawnPos, int viewId)
     {
-        object[] eventData = new object[]
+        object[] eventData =
         {
-            (int)spawner.SpawnerIndex,
+            spawner.SpawnerIndex,
             dataIndex,
             poolIndex,
             spawnPos,
             viewId
         };
 
-        RaiseEventOptions options = new RaiseEventOptions
-        {
-            Receivers = ReceiverGroup.Others,
-            CachingOption = EventCaching.DoNotCache
-        };
-
-        PhotonNetwork.RaiseEvent(ActivateEventCode, eventData, options, SendOptions.SendReliable);
+        PhotonNetwork.RaiseEvent(
+            ActivateEventCode,
+            eventData,
+            new RaiseEventOptions
+            {
+                Receivers = ReceiverGroup.Others,
+                CachingOption = EventCaching.DoNotCache
+            },
+            SendOptions.SendReliable
+        );
     }
 
+    // 적 반환 브로드캐스트
     public void BroadcastReturn(int dataIndex, int poolIndex)
     {
-        object[] eventData = new object[]
+        object[] eventData =
         {
-            (int)spawner.SpawnerIndex,
+            spawner.SpawnerIndex,
             dataIndex,
             poolIndex
         };
 
-        RaiseEventOptions options = new RaiseEventOptions
-        {
-            Receivers = ReceiverGroup.Others,
-            CachingOption = EventCaching.DoNotCache
-        };
-
-        PhotonNetwork.RaiseEvent(ReturnEventCode, eventData, options, SendOptions.SendReliable);
+        PhotonNetwork.RaiseEvent(
+            ReturnEventCode,
+            eventData,
+            new RaiseEventOptions
+            {
+                Receivers = ReceiverGroup.Others,
+                CachingOption = EventCaching.DoNotCache
+            },
+            SendOptions.SendReliable
+        );
     }
 
+    // 특정 플레이어에게 생성 정보 전송
     public void SendActivateToPlayer(Player player, int dataIndex, int poolIndex, Vector3 spawnPos, int viewId)
     {
-        object[] eventData = new object[]
+        object[] eventData =
         {
-            (int)spawner.SpawnerIndex,
+            spawner.SpawnerIndex,
             dataIndex,
             poolIndex,
             spawnPos,
             viewId
         };
 
-        RaiseEventOptions options = new RaiseEventOptions
-        {
-            TargetActors = new int[] { player.ActorNumber }
-        };
-
-        PhotonNetwork.RaiseEvent(ActivateEventCode, eventData, options, SendOptions.SendReliable);
+        PhotonNetwork.RaiseEvent(
+            ActivateEventCode,
+            eventData,
+            new RaiseEventOptions
+            {
+                TargetActors = new[] { player.ActorNumber }
+            },
+            SendOptions.SendReliable
+        );
     }
 
-    private void BroadcastCurrentStateToOthers()
-    {
-        if (!PhotonNetwork.IsMasterClient)
-            return;
-
-        foreach (EnemyData data in spawner.EnemyDatas)
-        {
-            if (data == null)
-                continue;
-        }
-    }
-
+    // 이벤트 수신
     public void OnEvent(EventData photonEvent)
     {
+        if (!gameObject.activeInHierarchy)
+            return;
+
         if (photonEvent.Code == ActivateEventCode)
         {
-            object[] data = (object[])photonEvent.CustomData;
-
-            int sIndex = System.Convert.ToInt32(data[0]);
-            if (sIndex != spawner.SpawnerIndex)
+            object[] data = photonEvent.CustomData as object[];
+            if (data == null || data.Length < 5)
                 return;
 
-            int dataIndex = (int)data[1];
-            int poolIndex = (int)data[2];
+            int spawnerIndex = System.Convert.ToInt32(data[0]);
+            if (spawnerIndex != spawner.SpawnerIndex)
+                return;
+
+            int dataIndex = System.Convert.ToInt32(data[1]);
+            int poolIndex = System.Convert.ToInt32(data[2]);
             Vector3 spawnPos = (Vector3)data[3];
-            int viewId = (int)data[4];
+            int viewId = System.Convert.ToInt32(data[4]);
 
-            Debug.Log($"[Spawner_{spawner.SpawnerIndex}] ActivateEvent 수신: poolIndex={poolIndex}");
             spawner.ActivateEnemy(dataIndex, poolIndex, spawnPos, viewId);
+            return;
         }
-        else if (photonEvent.Code == ReturnEventCode)
-        {
-            object[] data = (object[])photonEvent.CustomData;
 
-            int sIndex = System.Convert.ToInt32(data[0]);
-            if (sIndex != spawner.SpawnerIndex)
+        if (photonEvent.Code == ReturnEventCode)
+        {
+            object[] data = photonEvent.CustomData as object[];
+            if (data == null || data.Length < 3)
                 return;
 
-            int dataIndex = (int)data[1];
-            int poolIndex = (int)data[2];
+            int spawnerIndex = System.Convert.ToInt32(data[0]);
+            if (spawnerIndex != spawner.SpawnerIndex)
+                return;
+
+            int dataIndex = System.Convert.ToInt32(data[1]);
+            int poolIndex = System.Convert.ToInt32(data[2]);
 
             spawner.ReturnEnemyByIndex(dataIndex, poolIndex);
         }
