db6299575e72c8a7c96ee3f49bdcbe260bfc2e33
mntjksn <mntjksn@naver.com>
2026-04-27 18:33:32 +0900

    장착 아이템 저장 데이터

--- Assets/05 Inventory/Scripts/Data/EquipmentSaveData.cs
+++ Assets/05 Inventory/Scripts/Data/EquipmentSaveData.cs
@@ -1,10 +1,11 @@
 using System;
 
+// 장착 아이템 저장 데이터
 [Serializable]
 public class EquipmentSaveData
 {
-    public string weaponItemId;
-    public string armorItemId;
-    public string shoesItemId;
-    public string shieldItemId;
+    public string weaponItemId;   // 무기
+    public string armorItemId;    // 갑옷
+    public string shoesItemId;    // 신발
+    public string shieldItemId;   // 방패
 }
