e977cb2288cb5b7ad70b5a4f6e7a023c5a483a28
mntjksn <mntjksn@naver.com>
2026-04-27 18:39:20 +0900

    퀘스트 저장 데이터

--- Assets/09 Quest/Scripts/Data/QuestSaveData.cs
+++ Assets/09 Quest/Scripts/Data/QuestSaveData.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
 
+// 퀘스트 저장 데이터
 [Serializable]
 public class QuestSaveData
 {
-    public List<QuestStateData> questStates = new List<QuestStateData>();
+    public List<QuestStateData> questStates = new();
 }
