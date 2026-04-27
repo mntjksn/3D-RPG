3f936401a0576f7e7147f0b19460e43c2d6ba688
mntjksn <mntjksn@naver.com>
2026-04-27 18:32:18 +0900

    퀘스트 마크 상태

--- Assets/03 NPC/Scripts/Data/QuestMarkState.cs
+++ Assets/03 NPC/Scripts/Data/QuestMarkState.cs
@@ -1,7 +1,8 @@
+// 퀘스트 마크 상태
 public enum QuestMarkState
 {
-    None,
-    Available,
-    Progress,
-    Complete
+    None = 0,     // 표시 없음
+    Available,    // 수락 가능 (!)
+    Progress,     // 진행 중 (...)
+    Complete      // 완료 (?)
 }
