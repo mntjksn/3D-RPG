3618ddde7cafcfb56be83eeb42e8535f7fbbd22d
mntjksn <mntjksn@naver.com>
2026-04-27 18:31:19 +0900

    적 행동 가능 여부를 중앙에서 관리 (이동/공격 잠금)

--- Assets/02 Enemy/Scripts/EnemyActionLock.cs
+++ Assets/02 Enemy/Scripts/EnemyActionLock.cs
@@ -1,45 +1,35 @@
 using UnityEngine;
 
+// 적 행동 가능 여부를 중앙에서 관리 (이동/공격 잠금)
 public class EnemyActionLock : MonoBehaviour
 {
     public bool CanMove { get; private set; }
     public bool CanAttack { get; private set; }
-
     public bool IsMoving { get; private set; }
     public bool IsAttacking { get; private set; }
 
+    // 생성/부활 직후 - 부활 애니메이션이 끝나기 전까지 모든 행동 잠금
     public void ResetToSpawnState()
     {
-        // 생성/부활 직후: 부활 애니메이션 끝나기 전까지 잠금
-        CanMove = false;
-        CanAttack = false;
-
-        IsMoving = false;
-        IsAttacking = false;
+        CanMove = CanAttack = IsMoving = IsAttacking = false;
     }
 
+    // 이동/공격 잠금 (사망, 피격 경직 등)
     public void LockRecoverControls()
     {
-        CanMove = false;
-        CanAttack = false;
+        CanMove = CanAttack = false;
     }
 
+    // 이동/공격 해제
     public void UnlockRecoverControls()
     {
-        CanMove = true;
-        CanAttack = true;
+        CanMove = CanAttack = true;
     }
 
-    public void SetMove(bool value)
-    {
-        IsMoving = value;
-    }
+    public void SetMove(bool value) => IsMoving = value;
+    public void SetAttack(bool value) => IsAttacking = value;
 
-    public void SetAttack(bool value)
-    {
-        IsAttacking = value;
-    }
-
+    // 사망 시 호출 - 행동 잠금 및 상태 초기화
     public void OnDie()
     {
         LockRecoverControls();
@@ -47,7 +37,7 @@ public class EnemyActionLock : MonoBehaviour
         IsAttacking = false;
     }
 
-    // 부활 애니메이션 끝날 때 Animation Event
+    // 부활 애니메이션 종료 시 Animation Event로 호출
     public void OnRecoverFinished()
     {
         UnlockRecoverControls();
