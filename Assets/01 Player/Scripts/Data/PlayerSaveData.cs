83b680edc6ca84e5f8549ac1cefcc2b4f154721f
mntjksn <mntjksn@naver.com>
2026-04-27 18:29:09 +0900

    플레이어 전체 저장 데이터

--- Assets/01 Player/Scripts/Data/PlayerSaveData.cs
+++ Assets/01 Player/Scripts/Data/PlayerSaveData.cs
@@ -1,16 +1,17 @@
 using System.Collections.Generic;
 
+// 플레이어 전체 저장 데이터
 [System.Serializable]
 public class PlayerSaveData
 {
-    public int level;
-    public int currentExp;
-    public float currentHp;
-    public int gold;
+    public int level;          // 현재 레벨
+    public int currentExp;     // 현재 경험치
+    public float currentHp;    // 현재 체력
+    public int gold;           // 골드
 
-    public List<InventoryItemSaveData> inventoryItems = new List<InventoryItemSaveData>();
-    public EquipmentSaveData equipmentData = new EquipmentSaveData();
-    public PotionSlotSaveData potionSlot = new PotionSlotSaveData();
-    public UpgradeSaveData upgradeData = new UpgradeSaveData();
-    public QuestSaveData questData = new QuestSaveData();
+    public List<InventoryItemSaveData> inventoryItems = new();   // 인벤토리
+    public EquipmentSaveData equipmentData = new();              // 장비
+    public PotionSlotSaveData potionSlot = new();                // 포션 슬롯
+    public UpgradeSaveData upgradeData = new();                  // 업그레이드
+    public QuestSaveData questData = new();                      // 퀘스트
 }
