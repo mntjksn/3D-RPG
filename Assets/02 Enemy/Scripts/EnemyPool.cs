403a352bd7946e570a1f86a472cb1b44e7107fab
mntjksn <mntjksn@naver.com>
2026-04-27 18:32:03 +0900

    적 오브젝트 풀 참조 컴포넌트 - 스포너에 자신을 반환하는 역할

--- Assets/02 Enemy/Scripts/EnemyPool.cs
+++ Assets/02 Enemy/Scripts/EnemyPool.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+// 적 오브젝트 풀 참조 컴포넌트 - 스포너에 자신을 반환하는 역할
 public class EnemyPool : MonoBehaviour
 {
     private EnemySpawner ownerSpawner;
@@ -12,21 +13,15 @@ public class EnemyPool : MonoBehaviour
         enemyData = data;
     }
 
-    public EnemyData GetEnemyData()
-    {
-        return enemyData;
-    }
+    public EnemyData GetEnemyData() => enemyData;
+    public Vector3 GetLastSpawnPosition() => lastSpawnPosition;
 
     public void SetLastSpawnPosition(Vector3 pos)
     {
         lastSpawnPosition = pos;
     }
 
-    public Vector3 GetLastSpawnPosition()
-    {
-        return lastSpawnPosition;
-    }
-
+    // 사망 후 스포너의 풀로 반환
     public void ReturnToPool()
     {
         ownerSpawner?.ReturnEnemy(this);
