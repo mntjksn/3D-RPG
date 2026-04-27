83201ab56b6d1752f9e6fbad2fb10faebd4dfd1b
mntjksn <mntjksn@naver.com>
2026-04-27 18:32:30 +0900

    퀘스트 상태에 따라 NPC 머리 위 마크 표시

--- Assets/03 NPC/Scripts/QuestMarkView.cs
+++ Assets/03 NPC/Scripts/QuestMarkView.cs
@@ -1,14 +1,15 @@
 using TMPro;
 using UnityEngine;
 
+// 퀘스트 상태에 따라 NPC 머리 위 마크 표시
 public class QuestMarkView : MonoBehaviour
 {
     [SerializeField] private TMP_Text markText;
 
+    // 상태에 따라 마크 변경
     public void SetState(QuestMarkState state)
     {
-        if (markText == null)
-            return;
+        if (markText == null) return;
 
         switch (state)
         {
@@ -17,19 +18,23 @@ public class QuestMarkView : MonoBehaviour
                 break;
 
             case QuestMarkState.Available:
-                markText.gameObject.SetActive(true);
-                markText.text = "!";
+                SetMark("!");
                 break;
 
             case QuestMarkState.Progress:
-                markText.gameObject.SetActive(true);
-                markText.text = "...";
+                SetMark("...");
                 break;
 
             case QuestMarkState.Complete:
-                markText.gameObject.SetActive(true);
-                markText.text = "?";
+                SetMark("?");
                 break;
         }
+    }
+
+    // 공통 마크 처리
+    private void SetMark(string text)
+    {
+        markText.gameObject.SetActive(true);
+        markText.text = text;
     }
 }
