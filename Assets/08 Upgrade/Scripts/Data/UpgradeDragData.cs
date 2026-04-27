5d9477786626a14a3db84927c8af3ac011ad4898
mntjksn <mntjksn@naver.com>
2026-04-27 18:37:51 +0900

    업그레이드 UI 드래그 상태 공유 데이터

--- Assets/08 Upgrade/Scripts/Data/UpgradeDragData.cs
+++ Assets/08 Upgrade/Scripts/Data/UpgradeDragData.cs
@@ -1,22 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+// 업그레이드 UI 드래그 상태 공유 데이터
 public static class UpgradeDragData
 {
     public static ItemData DraggedItem;
     public static UpgradeInventorySlotUI SourceSlot;
+
     public static GameObject DragIconObject;
     public static Image DragIconImage;
 
+    // 드래그 상태 초기화
     public static void Clear()
     {
         DraggedItem = null;
         SourceSlot = null;
 
+        // 드래그 아이콘 제거
         if (DragIconObject != null)
+        {
             Object.Destroy(DragIconObject);
+            DragIconObject = null;
+        }
 
-        DragIconObject = null;
         DragIconImage = null;
     }
 }
