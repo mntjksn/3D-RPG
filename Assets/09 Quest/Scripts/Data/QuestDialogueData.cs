f0db2511bb4d87779e23c87cd966f42782578d74
mntjksn <mntjksn@naver.com>
2026-04-27 18:39:13 +0900

    퀘스트 대사 데이터

--- Assets/09 Quest/Scripts/Data/QuestDialogueData.cs
+++ Assets/09 Quest/Scripts/Data/QuestDialogueData.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "QuestDialogueData", menuName = "Quest/Dialogue Data")]
+// 퀘스트 대사 데이터
 public class QuestDialogueData : ScriptableObject
 {
     [Header("ID")]
@@ -9,5 +10,5 @@ public class QuestDialogueData : ScriptableObject
 
     [Header("대사 목록")]
     [TextArea(2, 5)]
-    public List<string> lines = new List<string>();
+    public List<string> lines = new();
 }
