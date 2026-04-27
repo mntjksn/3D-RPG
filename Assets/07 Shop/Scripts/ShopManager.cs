313bc6a004ed1d1209005ae2c8eb7c9bc26529da
mntjksn <mntjksn@naver.com>
2026-04-27 18:37:07 +0900

    상점 거래 성공 이벤트 관리

--- Assets/07 Shop/Scripts/ShopManager.cs
+++ Assets/07 Shop/Scripts/ShopManager.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 
+// 상점 거래 성공 이벤트 관리
 public class ShopManager : MonoBehaviour
 {
     public static ShopManager Instance { get; private set; }
@@ -9,6 +10,7 @@ public class ShopManager : MonoBehaviour
 
     private void Awake()
     {
+        // 싱글톤 설정
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -18,6 +20,7 @@ public class ShopManager : MonoBehaviour
         Instance = this;
     }
 
+    // 거래 성공 알림
     public void NotifyTradeSuccess()
     {
         OnTradeSuccess?.Invoke();
