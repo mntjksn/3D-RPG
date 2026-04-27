937940a3fefdb11a1334114a282ca8821e9faf03
mntjksn <mntjksn@naver.com>
2026-04-27 18:41:02 +0900

    Photon 서버 연결 및 연결 상태 콜백 관리

--- Assets/11 Photon/Scripts/PhotonNetworkManager.cs
+++ Assets/11 Photon/Scripts/PhotonNetworkManager.cs
@@ -1,73 +1,74 @@
-using UnityEngine;
+using System;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine;
 
+// Photon 서버 연결 및 연결 상태 콜백 관리
 public class PhotonNetworkManager : MonoBehaviourPunCallbacks
 {
     public static PhotonNetworkManager Instance { get; private set; }
 
-    private System.Action<bool, string> _connectCallback;
-    private bool _pendingSuccess = false;
-    private string _pendingMsg = "";
+    private Action<bool, string> connectCallback;
+    private bool isConnecting;
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            // 씬 전환해도 연결 유지
-            PhotonNetwork.AutomaticallySyncScene = true;
-        }
-        else
+        // 싱글톤 설정
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
+            return;
         }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // 씬 전환 시 자동 동기화
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
-    private void Update()
+    // Photon 연결 시도
+    public void ConnectToPhoton(string nickname, Action<bool, string> callback)
     {
-        if (_pendingSuccess && _connectCallback != null)
+        connectCallback = callback;
+
+        if (PhotonNetwork.IsConnectedAndReady)
         {
-            _pendingSuccess = false;
-            var cb = _connectCallback;
-            _connectCallback = null;
-            cb.Invoke(true, _pendingMsg);
+            connectCallback?.Invoke(true, "멀티플레이 서버 연결 완료!");
+            connectCallback = null;
+            return;
         }
-    }
 
-    public void ConnectToPhoton(string nickname, System.Action<bool, string> callback)
-    {
-        _connectCallback = callback;
+        if (PhotonNetwork.IsConnected)
+        {
+            connectCallback?.Invoke(true, "멀티플레이 서버 연결 완료!");
+            connectCallback = null;
+            return;
+        }
+
+        if (isConnecting)
+            return;
+
+        isConnecting = true;
 
         PhotonNetwork.NickName = nickname;
         PhotonNetwork.GameVersion = "1.0";
         PhotonNetwork.ConnectUsingSettings();
-
-        Debug.Log($"Photon 연결 시도 중... NickName: {nickname}");
     }
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Photon 마스터 서버 연결 완료");
-        // JoinLobby 대신 바로 방 입장
-        _pendingSuccess = true;
-        _pendingMsg = "멀티플레이 서버 연결 완료!";
-    }
+        isConnecting = false;
 
-    public override void OnJoinedLobby()
-    {
-        Debug.Log("로비 입장 완료");
-        _pendingSuccess = true;
-        _pendingMsg = "멀티플레이 서버 연결 완료!";
+        connectCallback?.Invoke(true, "멀티플레이 서버 연결 완료!");
+        connectCallback = null;
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        Debug.LogWarning($"Photon 연결 끊김: {cause}");
-        _pendingSuccess = false;
-        var cb = _connectCallback;
-        _connectCallback = null;
-        cb?.Invoke(false, $"서버 연결 실패: {cause}");
+        isConnecting = false;
+
+        connectCallback?.Invoke(false, $"서버 연결 실패: {cause}");
+        connectCallback = null;
     }
 }
